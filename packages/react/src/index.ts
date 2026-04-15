b42c0fb4f924ce6b10df748d3b7bfa1f38cd0feb
Ender Bonnet <enbonnet@gmail.com>
2026-04-15 10:37:33 -0400

    feat: export core methods from react

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -6,3 +6,27 @@ export {
   useDevtools,
   useDevtoolsContext,
 } from "./RTKDevtoolsProvider";
+
+// Re-export core API so users only need @rtk-devtools/react
+export {
+  createRTKDevtools,
+  createDevtoolsMiddleware,
+} from "@rtk-devtools/core";
+export type {
+  RTKDevtoolsConfig,
+  RTKDevtoolsInstance,
+  DevtoolsMiddlewareConfig,
+  DevtoolsSnapshot,
+  DevtoolsQueryEntry,
+  DevtoolsMutationEntry,
+  DevtoolsStats,
+  TagDescription,
+  TagNode,
+  TimelineEvent,
+  TimelineEventType,
+  TimelineEventFilter,
+  ApiEndpointInfo,
+  QueryStatus,
+  EventRecorderInstance,
+  RTKQueryApi,
+} from "@rtk-devtools/core";
